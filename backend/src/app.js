238164d9da768352ecfa5b81c1cc5e9c01fd9571
Aditya <kumaradityap123@gmail.com>
2026-02-02 11:43:36 +0530

    backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { createServer } from "node:http"
+import { Server } from "socket.io"
+import mongoose from "mongoose"
+import cors from "cors"
+import { connectToSocket } from './controllers/socketManager.js'
+
+const app = express();
+const server = createServer(app)
+const io = connectToSocket(server)
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors())
+app.use(express.json({ limit: "40kb"}))
+app.use(express.urlencoded({ limit: "40kb, extended: true"}))
+
+
+app.get("/home", (req, res) => {
+    return res.json({"hello" : "world"})
+})
+
+const start = async() =>{
+    // app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://zoomclone:zoomclone%40123@cluster0.menhanx.mongodb.net/?appName=Cluster0")
+    console.log(`mongo connected DB host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () =>{
+        console.log("listening on port 8000")
+    }) 
+}
+
+start();
