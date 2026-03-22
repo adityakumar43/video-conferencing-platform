56554a3423c4c50cf2ea8b0d3dbf7c42f5dbf271
Aditya <kumaradityap123@gmail.com>
2026-03-22 10:50:10 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Landing from "./pages/landing"
+import Authentication from './pages/authentication'
+import { AuthProvider } from './contexts/AuthContext'
+import VideoMeet from './pages/VideoMeet'
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/auth' element={<Authentication />} />
+            <Route path='/:url' element={<VideoMeet/>} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+
+
+    </>
   )
 }
 
