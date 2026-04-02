d5176a1e0a0653933098e7502419e7c8c8ad5d8a
Aditya <kumaradityap123@gmail.com>
2026-04-03 01:56:41 +0530

    modified meetingModel

--- backend/src/models/meetingModel.js
+++ backend/src/models/meetingModel.js
@@ -1,9 +1,10 @@
+import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const meetingSchema = new Schema({
     user_id: { type : String },
     meetingCode : { type : String, required : true },
-    date : { type : date, default : Date.now, required : true}
+    date : { type : Date, default : Date.now, required : true}
 })
 
 const Meeting = mongoose.model("Meeting", meetingSchema)
