20193d1f25fa0aa377aec55a1e5c2a289d1bed48
Aditya <kumaradityap123@gmail.com>
2026-04-03 02:28:29 +0530

    ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import './App.css'
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import Landing from "./pages/landing"
-import Authentication from './pages/authentication'
-import { AuthProvider } from './contexts/AuthContext'
-import VideoMeet from './pages/VideoMeet'
-
-
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/Landing.jsx';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
+import History from './pages/history';
 
 function App() {
   return (
-    <>
+    <div className="App">
 
       <Router>
+
         <AuthProvider>
+
+
           <Routes>
-            <Route path='/' element={<Landing />} />
+
+            <Route path='/' element={<LandingPage />} />
+
             <Route path='/auth' element={<Authentication />} />
-            <Route path='/:url' element={<VideoMeet/>} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
+
       </Router>
-
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
